54853a401e320db60b97c5a5eab9b772b35004d2
Justin Hong <justinhong.j@gmail.com>
2016-02-07 17:02:52 -0800

    add simple form for adding new posts, display posts, functions for both

--- app.js
+++ app.js
@@ -2,5 +2,25 @@ angular.module('spreddit', [])
 .controller('MainCtrl', [
 	'$scope',
 	function($scope) {
-		$scope.test = 'Hello World!';
+		$scope.posts = [
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes: 8},
+			{title: 'post 3', upvotes: 12},
+			{title: 'post 4', upvotes: 2}
+		];
+
+		$scope.addPost = function() {
+			if(!$scope.title || $scope.title === '') {return;}
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0
+			});
+			$scope.title = '';
+			$scope.link = '';
+		};
+
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes += 1;
+		};
 	}]);
