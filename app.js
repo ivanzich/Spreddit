af5b54ea01bef278df71b8bac4b9a233e2153799
Justin Hong <justinhong.j@gmail.com>
2016-02-07 17:30:59 -0800

    add service for posts, inject into MainCtrl

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 angular.module('spreddit', [])
+.factory('posts', [function() {
+
+	var p = {
+		posts: []
+	};
+	return p;
+
+}]);
+
+angular.module('spreddit', [])
 .controller('MainCtrl', [
-	'$scope',
-	function($scope) {
-		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 8},
-			{title: 'post 3', upvotes: 12},
-			{title: 'post 4', upvotes: 2}
-		];
 
-		$scope.addPost = function() {
-			if(!$scope.title || $scope.title === '') {return;}
-			$scope.posts.push({
-				title: $scope.title,
-				link: $scope.link,
-				upvotes: 0
-			});
-			$scope.title = '';
-			$scope.link = '';
-		};
+'$scope',
+'posts',
+function($scope, posts) {
+	$scope.posts = [
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 8},
+		{title: 'post 3', upvotes: 12},
+		{title: 'post 4', upvotes: 2}
+	];
 
-		$scope.incrementUpvotes = function(post) {
-			post.upvotes += 1;
-		};
-	}]);
+	$scope.addPost = function() {
+		if(!$scope.title || $scope.title === '') {return;}
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
+
+}]);
